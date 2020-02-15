91052b35d43f6e0d9de73f816105db7eebfef2b9
Leonardo de Lima Alves <leonalves@msn.com>
2020-02-15 11:07:16 -0300

    Add files via upload

---
+++ models/User.js
@@ -0,0 +1,18 @@
+class User {
+
+    constructor(name, tplanos, email, telefone, adesao, renovacao, vencimento, mensalidade, tacad, fceu, admin){
+
+      this.name = name;
+      this.tplanos = tplanos;
+      this.email = email;
+      this.telefone = telefone;
+      this.adesao = adesao;
+      this.renovacao = renovacao;
+      this.vencimento = vencimento;
+      this.mensalidade = mensalidade;
+      this.tacad = tacad;
+      this.fceu = fceu;
+      this.admin = admin
+    }
+
+}
